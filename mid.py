ce9b99f8a570f8aa5c36e9a03935e03d9f6e0467
Rhyan La Rosa <rhyan.larosa.20@usjr.edu.ph>
2021-04-09 19:05:07 +0800

    Added PIL on Gaussian Smoothing

--- mid.py
+++ mid.py
@@ -1,5 +1,8 @@
 import PIL, PIL.Image as I, PIL.ImageFilter as ImageFilter
+from PIL import Image, ImageOps
 import numpy as np
+from skimage import io
+import skimage
 from matplotlib import pyplot as P
 
 """
@@ -159,51 +162,53 @@ P.show()
 # CODE FOR MANUAL GAUSSIAN FILTERING (TO BE MODIFIED)
 # Issues: must use Pillow for compatibility
 
-# imgFileName = input("Enter Image File Name: ")
-# try:
-#     f = open(imgFileName)
-#     userMaskX = input("Enter Mask: ")
-#     userMaskY = input("by: ")
-#     userSigma = input("Enter Sigma: ")
-#     oldImage = skimage.io.imread(imgFileName)
-#     oldImage = skimage.color.rgb2gray(oldImage)# file opening
+imgFileName = input("Enter Image File Name: ")
+try:
+    f = open(imgFileName)
+    userMaskX = input("Enter Mask: ")
+    userMaskY = input("by: ")
+    userSigma = input("Enter Sigma: ")
+    oldImage = Image.open(imgFileName)
+    oldImage = ImageOps.grayscale(oldImage)
+    oldImage = np.array(oldImage)
+    # file opening
 
-#     def applyFilter (image, mask):
-#         row, col = image.shape
-#         m,n = mask.shape
-#         new = np.zeros((row+m-1, col+n-1))
-#         n = n//2
-#         m = m//2
-#         filteredImage = np.zeros(image.shape)
-#         new[m:new.shape[0] - m, n:new.shape[1] - n] = image
-#         for i in range(m, new.shape[0]- m):
-#             for j in range(n, new.shape[1]-n):
-#                 temp = new[i-m:i+m+1, j-m:j+m+1]
-#                 result = temp*mask
-#                 filteredImage[i-m,j-n] = result.sum()
+    def applyFilter (image, mask):
+        row, col = image.shape
+        m,n = mask.shape
+        new = np.zeros((row+m-1, col+n-1))
+        n = n//2
+        m = m//2
+        filteredImage = np.zeros(image.shape)
+        new[m:new.shape[0] - m, n:new.shape[1] - n] = image
+        for i in range(m, new.shape[0]- m):
+            for j in range(n, new.shape[1]-n):
+                temp = new[i-m:i+m+1, j-m:j+m+1]
+                result = temp*mask
+                filteredImage[i-m,j-n] = result.sum()
     
-#         return filteredImage# filter application
+        return filteredImage# filter application
 
-#     def gaussianFormula(m, n, sigma):
-#         gaussian = np.zeros((m,n))
-#         m = m//2
-#         n = n//2
-#         for x in range(-m, m+1):
-#             for y in range(-n, n+1):
-#                 x1 = sigma*(np.pi)**2
-#                 x2 = np.exp(-(x**2 + y**2)/(2*sigma**2))
-#                 gaussian[x+m, y+n] = (1/x1)*x2
-#         return gaussian# filter formula
+    def gaussianFormula(m, n, sigma):
+        gaussian = np.zeros((m,n))
+        m = m//2
+        n = n//2
+        for x in range(-m, m+1):
+            for y in range(-n, n+1):
+                x1 = sigma*(np.pi)**2
+                x2 = np.exp(-(x**2 + y**2)/(2*sigma**2))
+                gaussian[x+m, y+n] = (1/x1)*x2
+        return gaussian# filter formula
 
-#     newImage = applyFilter(oldImage, gaussianFormula(int(userMaskX),int(userMaskY),int(userSigma)))
-#     plt.imshow(oldImage,cmap = "gray")
-#     plt.title("Old Image")
-#     plt.figure()
-#     plt.imshow(newImage,cmap = "gray")
-#     plt.title("New Image")
-#     plt.show()
-# except IOError:
-#     print("File not accessible")
+    newImage = applyFilter(oldImage, gaussianFormula(int(userMaskX),int(userMaskY),int(userSigma)))
+    P.imshow(oldImage,cmap = "gray")
+    P.title("Old Image")
+    P.figure()
+    P.imshow(newImage,cmap = "gray")
+    P.title("New Image")
+    P.show()
+except IOError:
+    print("File not accessible")
 
 
 
