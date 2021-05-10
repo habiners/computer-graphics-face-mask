c8e57f70d6079539b7f1e55f261860526844acc2
Rhyan La Rosa <rhyan.larosa.20@usjr.edu.ph>
2021-05-10 16:57:26 +0800

    Added normalized csv

---
+++ normalizer.py
@@ -0,0 +1,10 @@
+from sklearn import preprocessing
+import pandas as pd
+
+first_csv = pd.read_csv("result.csv")
+scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1))
+names = first_csv.columns
+d = scaler.fit_transform(first_csv)
+scaled_df = pd.DataFrame(d, columns=names)
+scaled_df.head()
+scaled_df.to_csv(r'normalized.csv', index = False)
