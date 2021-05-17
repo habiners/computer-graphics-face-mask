7d3e5ddd4d78d8f25d415a8261f2a08a054c2935
Javin <javin.tan.20@usjr.edu.ph>
2021-05-18 00:52:29 +0800

    Sent image uri to flask todo: convert imguri to pyimage
    title

--- face-mask-recognition/src/app/main/main.component.ts
+++ face-mask-recognition/src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ImagePickerConf } from 'ngp-image-picker';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
@@ -8,9 +8,10 @@ import { ImagePickerConf } from 'ngp-image-picker';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   imgUrl: string = "";
+  verdict: JSON | undefined;
   ngOnInit(): void {
   }
   onSelectFile(event: any) {
@@ -22,6 +23,14 @@ export class MainComponent implements OnInit {
         this.imgUrl = eventer.target.result;
       };
       console.log(reader)
+    }
   }
+
+  async yeet(){
+    console.log(this.imgUrl);
+    // this.sentiment = await this.httpClient.get('http://127.0.0.1:5002/sentiment-analysis/' + this.cleaned).toPromise() as JSON;
+    this.verdict = await this.httpClient.get('http://127.0.0.1:5002/mask-detector?imguri=' + this.imgUrl).toPromise() as JSON;
+    // this.verdict = await this.httpClient.post('http://127.0.0.1:5002/mask-detector/' + this.imgUrl).toPromise() as JSON;
+    console.log(this.verdict);
   }
 }
--- flask_main.py
+++ flask_main.py
@@ -8,9 +8,17 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 from flask_jsonpify import jsonify
+from flask import request
 
-from testing.py import mask_detector
+from testing import mask_detector
+from Preprocessing import displayImage
 
+from PIL import Image
+import io, base64
+from binascii import a2b_base64
+from base64 import b64decode
+
+from urllib import request
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
@@ -19,11 +27,30 @@ CORS(app)
 def greet():
     return jsonify({"text": "Welcome to the Mask Detector Web Service!"})
 
+# https://stackoverflow.com/questions/33870538/how-to-parse-data-uri-in-python
+# https://stackoverflow.com/questions/31410525/base64-uri-to-png-python
+# https://stackoverflow.com/questions/24892035/how-can-i-get-the-named-parameters-from-a-url-using-flask
+# https://stackoverflow.com/questions/19395649/python-pil-create-and-save-image-from-data-uri
 class Mask_Detector(Resource):
-  def get(self, filename):
-    return jsonify({"result": mask_detector(filename)})
+  def get(self):
+    # print("NAKA SULOD")
+    # print(request.args.get('imguri'))
+    uri = request.args.get('imguri')
+    with request.urlopen(uri) as response:
+        data = response.read()
 
-api.add_resource(Mask_Detector, '/mask-detector/<filename>')
+    print(data)
+    # binary_data = a2b_base64(uri)
+    # print(binary_data)
+    image = Image.open(data)
+    displayImage(image)
+
+    # image = Image.open(io.BytesIO(base64.b64decode(uri.split(',')[1])))
+    # displayImage(image)
+    # return jsonify({"result": mask_detector(uri)})
+    return jsonify({"test": "YEET"})
+
+api.add_resource(Mask_Detector, '/mask-detector')
 
 if __name__ == "__main__":
     app.run(port=5002)
